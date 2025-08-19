6b09a2b655dde39dd9a9c4d5f91a69081174629c
zero42-042 <poxzer4444@gmail.com>
2025-08-19 22:42:28 +0300

    Update depends.sh

--- depends.sh
+++ depends.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #needed depends
-sudo pacman -S kitty dmenu xorg xorg-xinit feh xcompmgr flatpak git wget curl flameshot telegram-desktop steam discord pavucontrol nwg-look
+sudo pacman -S kitty dmenu xorg xorg-xinit feh xcompmgr flatpak git wget curl flameshot telegram-desktop steam discord pavucontrol nwg-look jdk8-openjdk jdk11-openjdk jdk17-openjdk jdk21-openjdk jdk-openjdk
 
 #flatpaki 💀💀
 flatpak install sober -y
@@ -17,7 +17,22 @@ cd
 rm -rf yay
 
 #paketiki net ne s geroinom
-yay -S zen-browser-bin tokyonight-gtk-theme
+yay -S zen-browser-bin tokyonight-gtk-theme polymc mc 
+
+#peremeshenie pidorov oy konfigov
+mv .xinitrc ~/
+mv patches.h ~/.dwm
+mv config.h ~/.dwm
+mkdir ~/.config/wlogout
+mv layout ~/.config/wlogout/
+mv info.sh ~/.dwm
+
+#DLYA RABOTI SKRIPTA
+sudo pacman -S xkb-switch      # Для определения раскладки клавиатуры
+sudo pacman -S pipewire-pulse  # Для управления звуком (pactl)
+sudo pacman -S coreutils       # Базовые утилиты (обычно уже установлен)
+sudo pacman -S util-linux      # Для free и других утилит (обычно уже установлен)
+sudo pacman -S procps-ng       # Для pgrep (обычно уже установлен)
 
 echo "Done!"
 # blya budet smeshno esli ne zarabotaet
