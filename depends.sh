11d5b10156436562cc57c1a507d2e53b69b93847
zero42-042 <poxzer4444@gmail.com>
2025-08-21 11:33:39 +0300

    Update depends.sh

--- depends.sh
+++ depends.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
 
 #needed depends
-sudo pacman -S i3lock ttf-jetbrains-mono kitty dmenu xorg xorg-xinit feh xcompmgr flatpak git wget curl flameshot telegram-desktop steam discord pavucontrol nwg-look jdk8-openjdk jdk11-openjdk jdk17-openjdk jdk21-openjdk jdk-openjdk mpv
+sudo pacman -S ly i3lock ttf-jetbrains-mono kitty dmenu xorg xorg-xinit feh xcompmgr flatpak git wget curl flameshot telegram-desktop steam discord pavucontrol nwg-look jdk8-openjdk jdk11-openjdk jdk17-openjdk jdk21-openjdk jdk-openjdk mpv
 
 #flatpaki 💀💀
 flatpak install sober -y
 
 #kopirka
-git clone https://github.com/bakkeby/dwm-flexipatch.git .dwm
+git clone https://github.com/bakkeby/dwm-flexipatch.git ~/.dwm
 git clone https://aur.archlinux.org/yay.git
 
 #yay nihuya ne yay eto blya hz kak opisar
 cd yay
 makepkg -si
-cd
+cd ..
 rm -rf yay
 
 #paketiki net ne s geroinom
 yay -S zen-browser-bin tokyonight-gtk-theme polymc mc 
 
+#delaem raznie shtuki
+chmod +x .xinitrc
+chmod +x info.sh
+chmod +x .fehbg
+chmod +x .xcompmgr
+
 #peremeshenie pidorov oy konfigov
+mv .fehbg ~/
+mv .xcompmgr ~/
 mv .xinitrc ~/
 mv patches.h ~/.dwm
 mv config.h ~/.dwm
@@ -33,11 +41,19 @@ mv startup.wav ~/.dwm/
 mv logoff.wav ~/.dwm/
 
 #DLYA RABOTI SKRIPTA
-sudo pacman -S xkb-switch      # Для определения раскладки клавиатуры
+yay -S xkb-switch      # Для определения раскладки клавиатуры
 sudo pacman -S pipewire-pulse  # Для управления звуком (pactl)
 sudo pacman -S coreutils       # Базовые утилиты (обычно уже установлен)
 sudo pacman -S util-linux      # Для free и других утилит (обычно уже установлен)
 sudo pacman -S procps-ng       # Для pgrep (обычно уже установлен)
+
+#drochim login manager
+sudo systemctl enable ly
+
+#ustanovka zapreta
+sudo git clone https://github.com/Sergeydigl3/zapret-discord-youtube-linux.git /opt/zapret
+cd /opt/zapret
+sudo bash main_script.sh
 
 echo "Done!"
 # blya budet smeshno esli ne zarabotaet
